8d35b3e4168aa7163df626b679c85ae7cc04c07a
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-08-18 23:07:58 +0530

    Improve input validation by resetting negative/NaN values to 0 in CompoundInterestCalculator

--- CompoundInterestCalculator/index.js
+++ CompoundInterestCalculator/index.js
@@ -8,10 +8,17 @@ function calculateCompoundInterest() {
     let rate = Number(rateInput.value / 100);
     let time = Number(timeInput.value);
 
-    if (principal < 0 || rate < 0 || time < 0) {
+    if (principal < 0 || isNaN(principal)) {
         principal = 0;
+        principalInput.value = 0;
+    }
+    if (rate < 0 || isNaN(rate)) {
         rate = 0;
+        rateInput.value = 0;
+    }
+    if (time < 0 || isNaN(time)) {
         time = 0;
+        timeInput.value = 0;
     }
 
     const amount = principal * Math.pow((1 + rate / 1), 1 * time);
