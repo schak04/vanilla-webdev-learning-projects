82868cfd293bffdd0ada2e7292317add909d79c3
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-08-20 19:19:30 +0530

    Add digital clock

---
+++ DigitalClock/index.js
@@ -0,0 +1,11 @@
+function runClock() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeStr = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeStr;
+}
+
+runClock();
+setInterval(runClock, 1000);
